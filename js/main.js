e795ae8c566cd7a6de3c4eab76864068e0791fec
ian <clarksware24@gmail.com>
2018-12-03 20:47:40 -0700

    add edit functions and delete functions

--- js/main.js
+++ js/main.js
@@ -44,6 +44,10 @@ function addcard(){
   document.getElementById("form").style.visibility = "visible";
 }
 
+function edit(){
+  document.getElementById("eform").style.visibility = "visible";
+}
+
 function createcard(){
   event.preventDefault();
   title = document.getElementById("card-t").value
@@ -64,4 +68,34 @@ function closeform(){
   document.getElementById("card-a").value = ""
   document.getElementById("form").style.visibility = "hidden";
 
+}
+
+function closeeform(){
+  event.preventDefault();
+  document.getElementById("ecard-t").value = ""
+  document.getElementById("ecard-q").value = ""
+  document.getElementById("ecard-a").value = ""
+  document.getElementById("eform").style.visibility = "hidden";
+
+}
+
+function editcard(){
+  event.preventDefault();
+  title = document.getElementById("ecard-t").value
+  question = document.getElementById("ecard-q").value
+  answer = document.getElementById("ecard-a").value
+  var ecard = { title, question, answer }
+  var index =  cardsList.indexOf(chosen)
+  cardsList[index] = ecard
+  document.getElementById("ecard-t").value = ""
+  document.getElementById("ecard-q").value = ""
+  document.getElementById("ecard-a").value = ""
+  document.getElementById("eform").style.visibility = "hidden";
+  newcard()
+}
+
+function deletecard(){
+  event.preventDefault();
+  cardsList.shift(chosen)
+  newcard()
 }
